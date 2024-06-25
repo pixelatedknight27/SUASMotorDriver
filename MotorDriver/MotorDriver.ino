b841485b0a11a396a5c25e04841a0fcada25d43f
pixelatedknight27 <maxgross72@gmail.com>
2024-06-25 11:19:30 -0700

    WORKS!!!!!!

--- MotorDriver/MotorDriver.ino
+++ MotorDriver/MotorDriver.ino
@@ -1,6 +1,7 @@
 #include <SPI.h>
 #include <RH_NRF24.h>
 #include <L298N.h>
+#include <String.h>
  
 // Singleton instance of the radio driver
 #define CE_PIN 7
@@ -10,15 +11,19 @@ RH_NRF24 nrf24(CE_PIN, CSN_PIN);
 
 L298N myMotors[5] = {
   L298N(5, 30, 31),   // driver1
-  L298N(3, 28, 29),   // driver2
-  L298N(9, 34, 35),   // driver3
+  L298N(2, 35, 36),   // driver2
+  L298N(3, 29, 30),   // driver3
   L298N(11, 32, 33),  // driver4
   L298N(13, 24, 25)   // driver5
 };
 
+bool isRunning[] = {0,0,0,0,0};
+
 void setup() {
   // put your setup code here, to run once:
   for(L298N motor : myMotors){
+    motor.forward();
+    delay(1000);
     motor.stop();
   }
 
@@ -48,12 +53,14 @@ void setup() {
   }
 }
 
-void getMotorAction(L298N motor){
-  if(motor.isMoving()){
+void getMotorAction(L298N motor, int motorNum){
+  if(isRunning[motorNum]){
     motor.stop();
+    isRunning[motorNum] = 0;
   }
   else{
     motor.forward();
+    isRunning[motorNum] = 1;
   }
 }
 
@@ -74,12 +81,20 @@ void loop() {
       Serial.print("Got request: " + msg);
 
       // if the substring "Button" is found in the message
-      if(msg.find("Button")){
-        // look at 7th position of message - this is the motor number to activate.
-        // i.e. Button4
-        //            ^
-        // activate the motor at position 3 in the list (starting from 0)
-        getMotorAction(myMotors[(int)msg[6]-1]);
+      if(msg == "Button1"){
+        getMotorAction(myMotors[0],0);
+      }
+      else if(msg == "Button2"){
+        getMotorAction(myMotors[1],1);
+      }
+      else if(msg == "Button3"){
+        getMotorAction(myMotors[2],2);
+      }
+      else if(msg == "Button4"){
+        getMotorAction(myMotors[3],3);
+      }
+      else if(msg == "Button5"){
+        getMotorAction(myMotors[4],4);
       }
       else{
         Serial.println("invalid command");
